92302e48f2235fae0bbd14030138461b445a007d
MOENS Alexandre <mao@odoo.com>
2018-11-17 18:30:56 +0100

    [REF] retabed
    replaced tabs indent with 4 spaces

--- zsh_alias.sh
+++ zsh_alias.sh
@@ -3,18 +3,18 @@
 ##############################################
 
 maj(){
-	sudo apt-get update && 
-	sudo apt-get upgrade -y && 
-	sudo apt-get autoclean && 
-	sudo apt-get autoremove -y
+    sudo apt-get update && 
+    sudo apt-get upgrade -y && 
+    sudo apt-get autoclean && 
+    sudo apt-get autoremove -y
 }
 
 fullmaj(){
-	sudo apt-get update &&
-	sudo apt-get upgrade -y &&
-	sudo apt-get dist-upgrade -y &&
-	sudo apt-get autoclean &&
-	sudo apt-get autoremove -y
+    sudo apt-get update &&
+    sudo apt-get upgrade -y &&
+    sudo apt-get dist-upgrade -y &&
+    sudo apt-get autoclean &&
+    sudo apt-get autoremove -y
 }
 
 alias reload_zshrc='source ~/.zshrc'
@@ -22,15 +22,15 @@ alias reload_zshrc='source ~/.zshrc'
 alias cya='systemctl suspend -i'
 
 clear_ram(){
-	echo "This is going to take a while ..." && 
-	echo "Droppping cache" && 
-	sudo su -c "echo 3 > /proc/sys/vm/drop_caches" root && 
-	echo "Cache dropped" && 
-	echo "turning swap off" && 
-	sudo swapoff -a && 
-	echo "turning swap back on" && 
-	sudo swapon -a && 
-	echo "Aaaaaand... done!" 
+    echo "This is going to take a while ..." && 
+    echo "Droppping cache" && 
+    sudo su -c "echo 3 > /proc/sys/vm/drop_caches" root && 
+    echo "Cache dropped" && 
+    echo "turning swap off" && 
+    sudo swapoff -a && 
+    echo "turning swap back on" && 
+    sudo swapon -a && 
+    echo "Aaaaaand... done!" 
 }
 
 noweb(){
@@ -48,41 +48,41 @@ DEFAULT_USER='odoo'
 alias e="vim"
 
 eza(){
-	era vim $1
+    era vim $1
 }
 
 geza(){
-	era gedit $1
+    era gedit $1
 }
 
 era(){
-	# edit and reload alias
-	# era [vim|gedit] the_alias_file_to_edit
-	local file_to_load=" "
-	case $2 in
-		zsh)
-			file_to_load="zsh_alias.sh"
-			;;
-		loader)
-			file_to_load="alias_loader.sh"
-			;;
-		odoo)
-			file_to_load="odoo_alias.sh"
-			;;
-		*)
-			#default
-			file_to_load="odoo_alias.sh"
-			;;
-	esac
+    # edit and reload alias
+    # era [vim|gedit] the_alias_file_to_edit
+    local file_to_load=" "
+    case $2 in
+        zsh)
+            file_to_load="zsh_alias.sh"
+            ;;
+        loader)
+            file_to_load="alias_loader.sh"
+            ;;
+        odoo)
+            file_to_load="odoo_alias.sh"
+            ;;
+        *)
+            #default
+            file_to_load="odoo_alias.sh"
+            ;;
+    esac
 
-	if [ "$1" = "vim" ]
-	then
-		e $AP/$file_to_load &&
-		reload_zshrc
-	else
-		gedit $AP/$file_to_load &&
-		reload_zshrc
-	fi
+    if [ "$1" = "vim" ]
+    then
+        e $AP/$file_to_load &&
+        reload_zshrc
+    else
+        gedit $AP/$file_to_load &&
+        reload_zshrc
+    fi
 }
 
 
@@ -94,9 +94,9 @@ era(){
 
 #history analytics
 history_count(){
-	history -n | cut -d' ' -f1 | sort | uniq -c | trim | sort -g | tac | less
+    history -n | cut -d' ' -f1 | sort | uniq -c | trim | sort -g | tac | less
 }
 trim(){
-	awk '{$1=$1};1'
+    awk '{$1=$1};1'
 }
 
