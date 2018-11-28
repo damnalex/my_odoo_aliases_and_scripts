b9826d4e263b4bd715cd17e49e29e38f149798ae
MOENS Alexandre <mao@odoo.com>
2018-11-28 14:37:20 +0100

    [TYPO]

--- odoo_alias.sh
+++ odoo_alias.sh
@@ -50,7 +50,7 @@ git_branch_info(){
     local branch_version="$(git_branch_version $1)"
     local branch_late=$(git -C $1 cherry $branch_version origin/$branch_version | wc -l)
     local branch_ahead=$(git -C $1 cherry origin/$branch_version $branch_version | wc -l)
-    echo "$branch_version        ↓ $branch_late ↑ $branch_ahead"
+    echo "$branch_version \t\t↓ $branch_late ↑ $branch_ahead"
 }
 
 golist(){
