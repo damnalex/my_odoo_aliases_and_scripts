d14e87c90c5c883755c48b593eece54eb5967004
Moens Alexandre <mao@odoo.com>
2019-07-29 11:31:40 +0200

    [WIP] start_odoo : add base
    This adds a base struture to the script with completed usage configuration

--- python_scripts/start_odoo.py
+++ python_scripts/start_odoo.py
@@ -3,9 +3,22 @@
 start_odoo
 
 usage:
-    start_odoo <dbname> [--port <port_nb>] [--checkout --ptvsd] [-u <module> ... | -i <module> ...] [--saas] 
+    start_odoo <dbname> [--port <port_nb>] [--checkout --ptvsd] [-u <modules_to_update> ... | -i <modules_to_install> ...] [--saas] [--stop-after-init]
+
+options:
+    --port=<port_nb>
+    -u=<modules_to_update>
+    -i=<modules_to_install>
+
 
 """
 from docopt import docopt
 
+def main():
+    # args parsing
+    opt = docopt(__doc__)
+    print(opt)
 
+
+if __name__ == "__main__":
+    main()
