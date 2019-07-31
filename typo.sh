2b92ad723442f5c4438d0e84cb64fd91109df907
Moens Alexandre <mao@odoo.com>
2019-07-31 15:54:33 +0200

    [ADD] new typos
    added new typos to typo.sh and an explanation banner

--- typo.sh
+++ typo.sh
@@ -1,3 +1,7 @@
+#################################################################################################
+# a collection of recurring typo and short cut created quickly created vie the new_typo command #
+#################################################################################################
+
 alias 'gti'='git'
 alias 'pyhton3'='python3'
 alias 'pyhton'='python'
@@ -5,3 +9,6 @@ alias 'l$'='l'
 alias 'eos'='oes'
 alias 'ose'='oes'
 alias 'eaz'='eza'
+alias 'exiit'='exit'
+alias 'c'='clear'
+alias 'za'='eza'
