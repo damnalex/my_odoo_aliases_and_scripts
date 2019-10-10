89b1dba46ea29360315115eb0e3200110e1eef1e
Moens Alexandre <mao@odoo.com>
2019-10-10 17:01:55 +0200

    [ADD] _go_multi
    This adds the posibility to checkout different branches at the same time
    for the different repos using go
    
    the branches are in order : community, enterprise ,
    design-themes(optional), iternal(optional)

--- odoo_alias.sh
+++ odoo_alias.sh
@@ -17,12 +17,43 @@ clear_all_pyc(){
 #git
 alias git_odoo="$AP/python_scripts/git_odoo.py"
 go(){ #switch branch for all odoo repos
-    local version=$1
     echo "cleaning the junk"
     clear_pyc
-    git_odoo checkout $version
+    if [[ $# -gt 1 ]]
+    then
+        _go_multi $@
+    else
+        local version=$1
+        git_odoo checkout $version
+    fi
     echo "-------"
     golist
+}
+
+_go_multi(){
+    if [[ $# -ge 2 ]]
+    then
+        echo "checkouting community repo to $1"
+        git -C $ODOO checkout $1
+        echo "checkouting enterprise repo to $2"
+        git -C $ENTERPRISE checkout $2
+    fi
+    if [[ $# -ge 3 ]]
+    then
+        echo "checkouting design-themes repo to $3"
+        git -C $SRC/design-themes checkout $3
+    fi
+    if [[ $# -eq 4 ]]
+    then
+        echo "checkouting internal repo to $4"
+        git -C $INTERNAL checkout $4
+    fi
+    if [[ $# -gt 4 ]]
+    then
+        echo "too many params, ignoring the following:"
+        echo "$@[5, -1]"
+        return 1
+    fi
 }
 
 go_update_and_clean(){
