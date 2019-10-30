9ea47088dbcb21195c36fd412f6734d7a7bc1103
Moens Alexandre <mao@odoo.com>
2019-10-30 09:46:49 +0100

    [ADD] completion for go & go_update_and_clean
    also, there is a dirt fix for _complete_withB_on_index_A

--- completion.sh
+++ completion.sh
@@ -6,7 +6,10 @@ _complete_with_B_on_index_A() {
     local index=$1
     local funct=$2
     if [[ COMP_CWORD -eq $(($index)) ]]; then
-        eval $funct $@[3, -1]
+        eval $funct $3 $4 $5 $6 $7 $8 $9
+        # this is very ugly, but the completion is doing weird stuff with $@[x,-1]
+        # if I need more param, i'll just add more stuff here
+        # this also mean that a completion command called by _complete_with_B_on_index_A cannot call it back.
     fi
 }
 
@@ -21,6 +24,12 @@ _complete_db_name() {
 
 _complete_db_name_on_first_param() {
     _complete_with_B_on_index_A 1 _complete_db_name
+}
+
+_complete_branch_name_on_repo_A(){
+    local repo_path=$1
+    local branch_names=$(git -C $repo_path branch | tr '\n' ' ')
+    COMPREPLY=($(compgen -W "$branch_names" -- "${COMP_WORD[COMP_CWORD]}"))
 }
 
 ####################################
@@ -49,3 +58,17 @@ complete -o default -F _complete_db_name_on_first_param dropodoo
 complete -o default -F _complete_db_name_on_first_param lu
 complete -o default -F _complete_db_name_on_first_param psql
 complete -o default -F _complete_db_name_on_first_param pgcli
+
+_go() {
+    _complete_with_B_on_index_A 1 _complete_branch_name_on_repo_A $ODOO
+    _complete_with_B_on_index_A 2 _complete_branch_name_on_repo_A $ENTERPRISE
+    _complete_with_B_on_index_A 3 _complete_branch_name_on_repo_A $SRC/design-themes
+    _complete_with_B_on_index_A 4 _complete_branch_name_on_repo_A $INTERNAL
+}
+complete -o default -F _go go
+
+_go_update_and_clean() {
+    _complete_with_B_on_index_A 1 _complete_branch_name_on_repo_A $ODOO
+}
+complete -o default -F _go_update_and_clean go_update_and_clean
+
