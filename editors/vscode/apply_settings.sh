e7fdefd28e91a333ca4d31b82d90b728faec1c0f
Moens Alexandre <mao@odoo.com>
2019-11-22 13:02:45 +0100

    [IMP] vscode settings: don't keep 2 sources of truth
    make vscode use the file in this repo directly to sync the version
    controlled version of the setting and the "running" settings
    
    apply_settings.sh needs to only be called once
    
    update_settings.sh is not usefull anymore

--- editors/vscode/apply_settings.sh
+++ editors/vscode/apply_settings.sh
@@ -1,1 +1,3 @@
-cat $AP/editors/vscode/settings.json > ~/Library/Application\ Support/Code/User/settings.json
+cp $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.json_BAK
+rm $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $HOME/Library/Application\ Support/Code/User/settings.json $AP/editors/vscode/settings.json
--- editors/vscode/update_settings.sh
+++
@@ -1,3 +0,0 @@
-cat ~/Library/Application\ Support/Code/User/settings.json > $AP/editors/vscode/settings.json &&
-    git -C $AP add $AP/editors/vscode/settings.json &&
-    git -C $AP commit -m "[AUTOMATIC] update vscode settings.json"
