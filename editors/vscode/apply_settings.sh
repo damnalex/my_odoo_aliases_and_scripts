93efa8539833d97bf46b9a4fcb6bc7064807063e
Moens Alexandre <mao@odoo.com>
2020-01-13 17:05:16 +0100

    [FIX] vscode/apply_settings - ln wrong order

--- editors/vscode/apply_settings.sh
+++ editors/vscode/apply_settings.sh
@@ -1,3 +1,3 @@
 cp $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.json_BAK
 rm $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $HOME/Library/Application\ Support/Code/User/settings.json $AP/editors/vscode/settings.json
+ln -s $AP/editors/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json 
