458a2a1563b577619610277470270a7d64e98dc3
Moens Alexandre <mao@odoo.com>
2020-10-08 16:02:19 +0200

    [IMP] oes: optionally disable smart venv usage
    prefix the normal parameters with 'raw' to disable the automatic change
    of venv
    
    This is needed because some options suppported by 'fetch' does not work
    for 'start'.

--- odoo_alias.sh
+++ odoo_alias.sh
@@ -104,19 +104,23 @@ goso() {
 
 oes() {
     # start oe-support, with some smartness
-    if [[ $1 == "fetch" ]] && ! [[ $* == *'--no-start'* ]]; then
-        # running first a fetch without starting the db
-        # then running a separate start to automagically
-        # use the right virtual-env, even when the db version
-        # is not known beforehand
-        eval oes $@ --no-start
-        eval oes start $@[2,-1]
-        return
-    fi
-    if [[ $1 == "start" ]] || [[ $1 == "restore" ]]; then
-        local version=$(_db_version $(list_db_like "%$2")) 2>/dev/null
-        if [[ $version != "" ]]; then
-            go_venv $version
+    if [[ $1 == "raw" ]]; then
+        shift
+    else
+        if [[ $1 == "fetch" ]] && ! [[ $* == *'--no-start'* ]]; then
+            # running first a fetch without starting the db
+            # then running a separate start to automagically
+            # use the right virtual-env, even when the db version
+            # is not known beforehand
+            eval oes $@ --no-start
+            eval oes start $@[2,-1]
+            return
+        fi
+        if [[ $1 == "start" ]] || [[ $1 == "restore" ]]; then
+            local version=$(_db_version $(list_db_like "%$2")) 2>/dev/null
+            if [[ $version != "" ]]; then
+                go_venv $version
+            fi
         fi
     fi
     #start odoo support
