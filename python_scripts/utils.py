d5e3aee9b205be1d477263e6063ccaf560766f63
Moens Alexandre <mao@odoo.com>
2020-12-22 16:38:03 +0100

    [IMP] utils - env : raise early
    when no Environement variables is found.

--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -9,6 +9,8 @@ class EnvironmentExtractor:
     def __getattr__(self, name):
         """ Will only be called the first time self.XXX is called"""
         env_var = os.getenv(name)
+        if not env_var:
+            raise ValueError(f"No value found for environment variable {name}")
         setattr(self, name, env_var)
         return env_var
 
