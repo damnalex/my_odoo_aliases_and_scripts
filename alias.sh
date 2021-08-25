5397cb473ee176c3a71721bc20f2d7e5f3c44406
Moens Alexandre <mao@odoo.com>
2021-08-25 15:04:10 +0200

    [FIX] test-dump
    for some reason the neuter_db alias acts weird and doesn't work in
    shell functions

--- alias.sh
+++ alias.sh
@@ -687,5 +687,6 @@ sync_filestore_to_remote() {
 
 test-dump() {
     #create a DB using the dump.sql file in the current folder
-    createdb "$1-test" && psql -d "$1-test" < dump.sql  && neuter_db "$1-test" && pl
+    local $db_name="$1-test"
+    createdb $db_name && psql -d $db_name <dump.sql && $ST/tools/neuter.py $db_name && pl
 }
