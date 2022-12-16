f4a1a7b2634afa016a6701e5b69791c2e5088a3c
Moens Alexandre <mao@odoo.com>
2022-12-16 13:28:54 +0100

    tmp stuff

--- temporary-scripts.sh
+++ temporary-scripts.sh
@@ -1,1 +1,11 @@
 # nothing to see here
+update_my_beta(){
+    local st_branch=$(git -C $ST rev-parse --abbrev-ref HEAD)
+    if [[ $st_branch == "my_beta" ]]; then
+        git -C $ST fetch;
+        git -C $ST rebase origin/odoo-support-devtool-v2;
+        git -C $ST rebase origin/master;
+        return
+    fi
+    echo "not curently running 'my_beta' branch, no changes done "
+}
