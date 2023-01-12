3f71ced89abbd531e32725a548c344fb179e407c
Moens Alexandre <mao@odoo.com>
2023-01-12 13:49:47 +0100

    [AUTOFORMAT]

--- temporary-scripts.sh
+++ temporary-scripts.sh
@@ -1,9 +1,9 @@
 # nothing to see here
-update_my_beta(){
+update_my_beta() {
     local st_branch=$(git -C $ST rev-parse --abbrev-ref HEAD)
     if [[ $st_branch == "my_beta" ]]; then
-        git -C $ST fetch;
-        git -C $ST rebase origin/master;
+        git -C $ST fetch
+        git -C $ST rebase origin/master
         return
     fi
     echo "not curently running 'my_beta' branch, no changes done "
