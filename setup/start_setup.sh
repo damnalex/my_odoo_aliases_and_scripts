c916f65d1f9fbaffcc5dcec49993eb5fddf8058d
Alexandre MOENS <alex@Alexandres-MacBook-Pro.local>
2023-03-05 14:11:54 +0100

    setup update

--- setup/start_setup.sh
+++ setup/start_setup.sh
@@ -5,7 +5,7 @@ if [[ $1 == "base" ]]; then
     # install homebrew
     echo "Installing Homebrew"
     echo "-------------------"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # desktop apps
     echo "installing desktop apps"
     echo "-----------------------"
@@ -14,7 +14,7 @@ if [[ $1 == "base" ]]; then
     echo "setting up terminal to use Zsh and oh-my-zsh"
     echo "--------------------------------------------"
     brew install zsh zsh-completions
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     return
 fi
@@ -25,55 +25,6 @@ if [[ $1 == "step2" ]]; then
     echo "###################"
     source ../alias_loader.sh
 
-    # ODOO SETUP
-    run 15 echo " "
-    echo "Starting odoo setup"
-    echo "###################"
-    # echo "Installing required third party libs"
-    # echo "------------------------------------"
-    # TODO install wkhtmltopdf
-    # TODO install all requiered dev libs
-    run 15 echo " "
-    echo "Building Odoo repos"
-    echo "-------------------"
-    echo "Starting with the universe"
-    echo "°°°°°°°°°°°°°°°°°°°°°°°°°°"
-    mkdir -p "$SRC"
-    cd $SRC
-    git clone git@github.com:odoo/support-tools.git
-    git clone git@github.com:odoo/odoo.git
-    git -C $ODOO remote add git@github.com:odoo-dev/odoo.git
-    git clone git@github.com:odoo/enterprise.git
-    git -C $ENTERPRISE remote add git@github.com:odoo-dev/enterprise.git
-    git clone git@github.com:odoo/design-themes.git
-    git -C $SRC/design-themes remote add git@github.com:odoo-dev/design-themes.git
-    git clone git@github.com:odoo/internal.git
-    git clone git@github.com:odoo/documentation-user.git
-    run 15 echo " "
-    echo "Continuing with the multiverse"
-    echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-    mkdir -p "$SRC_MULTI"
-    cd $SRC_MULTI
-    mkdir master
-    cd master
-    git clone git@github.com:odoo/odoo.git
-    git -C $SRC_MULTI/odoo checkout master
-    git clone git@github.com:odoo/enterprise.git
-    git -C $SRC_MULTI/enterprise checkout master
-    git clone git@github.com:odoo/design-themes.git
-    git -C $SRC_MULTI/design-themes checkout master
-    run 15 echo " "
-    echo "Base setup for multiverse done: building main branches"
-    echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-    build_multiverse_branch 15.0
-    build_multiverse_branch 14.0
-    build_multiverse_branch 13.0
-    run 15 echo " "
-    echo "Main branches of the mutliverse done: building main virtualenvs"
-    echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"
-    build_odoo_virtualenv 15.0
-    build_odoo_virtualenv 14.0
-    build_odoo_virtualenv 13.0
 
     run 15 echo " "
     echo "Settings up editors"
