3f776a5078bdc9c5dbd8975b9b5d654cf8516768
Moens Alexandre <mao@odoo.com>
2024-06-05 14:14:26 +0200

    [NVIM] config: avoid short hands

--- editors/neovim/lua/core/general.lua
+++ editors/neovim/lua/core/general.lua
@@ -4,16 +4,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "-", vim.cmd.Ex)
 
 -- editor
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
-vim.o.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
 -- turn off search higlight on <leader><space>
 vim.keymap.set('n', '<leader> ', vim.cmd.nohlsearch, {})
 -- show search result in the middle of the screen
@@ -27,17 +27,17 @@ vim.keymap.set('n', 'g#', 'g#zz', {})
 vim.keymap.set('n', 'gV', '`[v`]', {})
 
 -- style
-vim.o.number = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.cursorline = true
-vim.o.wrap = false
-vim.o.scrolloff = 8
-vim.o.showcmd = true
-vim.o.list = true
+vim.opt.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.showcmd = true
+vim.opt.list = true
 -- with this, copying bits of code (with the mouse) includes dots as spaces,
 -- use "*y to cleanly (properly) copying to the clipboard
-vim.o.listchars = 'trail:+,tab:>-,nbsp:␣,space:.'
+vim.opt.listchars = 'trail:+,tab:>-,nbsp:␣,space:.'
 
 
 -- other remapping
@@ -46,4 +46,4 @@ vim.keymap.set('i', 'jjj', '<Esc>')
 vim.keymap.set('i', 'kkk', '<Esc>')
 
 -- other
-vim.o.wildmenu = true
+vim.opt.wildmenu = true
