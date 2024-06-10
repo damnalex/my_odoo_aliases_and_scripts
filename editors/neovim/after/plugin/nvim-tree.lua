c005025366628eb29e694a3796e0d0e98271e1cb
Moens Alexandre <mao@odoo.com>
2024-06-10 15:40:43 +0200

    [FIX] NVIM: nvim-tree X vim-fugitive
    :GBrowse would fail when netrw was fully disabled

--- editors/neovim/after/plugin/nvim-tree.lua
+++ editors/neovim/after/plugin/nvim-tree.lua
@@ -1,6 +1,8 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- nvim-tree's doc says: disable netrw at the very start of your init.lua
+-- but it looks to be working correctly without it anyway
+-- furthermore these changes broke the :GBrowse feature of vim-fugitive
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
 require("nvim-tree").setup()
