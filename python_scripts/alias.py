d088942d8c62a498e150ad39e9d3fbb29a51f453
Moens Alexandre <mao@odoo.com>
2024-10-01 11:05:29 +0200

    e. = .e

--- python_scripts/alias.py
+++ python_scripts/alias.py
@@ -985,7 +985,7 @@ def typos_and_simple_aliases():
             "clar",
         ],
         "new_typo": ["new_ypo", "new_tupo", "new_ytpo", "newtypo"],
-        "e .": ["e."],
+        "e .": ["e.", ".e"],
         "exit": [
             "exiit",
             "exit$",
