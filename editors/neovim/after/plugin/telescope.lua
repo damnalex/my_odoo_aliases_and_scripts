aacf1efe74e5a12403b0695b02fc4bcfd4a8a807
Moens Alexandre <mao@odoo.com>
2025-02-05 11:22:57 +0100

    [IMP] nvim: make telescope follow symlinks
    ... when searching files and grepping code
    
    This is a follow up to the universe-ectomy

--- editors/neovim/after/plugin/telescope.lua
+++ editors/neovim/after/plugin/telescope.lua
@@ -11,6 +11,12 @@ vim.keymap.set('n', '<leader>fr', tb.resume, {}) -- resume last picker (even if 
 
 local telescope = require("telescope")
 local ta = require("telescope.actions")
+
+-- follow symbolic links
+local telescopeConfig = require("telescope.config")
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+table.insert(vimgrep_arguments, "-L")
+
 telescope.setup({
     defaults = {
         mappings = {
@@ -18,8 +24,15 @@ telescope.setup({
                 ["<C-j>"] = ta.cycle_history_next,
                 ["<C-k>"] = ta.cycle_history_prev,
             }
-        }
-    }
+        },
+        vimgrep_arguments = vimgrep_arguments,
+    },
+    pickers = {
+        find_files = {
+            -- follow symlink in file search
+            find_command = { "rg", "--files", "-L" },
+        },
+    },
 })
 
 telescope.load_extension("live_grep_args")
