7e8371452873785322fd305886e456e3a3ddb535
Moens Alexandre <mao@odoo.com>
2025-08-08 15:43:53 +0200

    whatever

--- editors/neovim/init.lua
+++ editors/neovim/init.lua
@@ -343,56 +343,7 @@ require("lazy").setup({
                 -- CTRL-S is mapped in Insert mode to vim.lsp.buf.signature_help()
                 require'lspconfig'.ruff.setup{}
                 require'lspconfig'.pyright.setup{}
-                -- set luals to play nice in vim config file
-                 vim.lsp.config('lua_ls', {
-                   on_init = function(client)
-                     if client.workspace_folders then
-                       local path = client.workspace_folders[1].name
-                       if
-                         path ~= vim.fn.stdpath('config')
-                         and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-                       then
-                         return
-                       end
-                     end
-
-                     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                       runtime = {
-                         -- Tell the language server which version of Lua you're using (most
-                         -- likely LuaJIT in the case of Neovim)
-                         version = 'LuaJIT',
-                         -- Tell the language server how to find Lua modules same way as Neovim
-                         -- (see `:h lua-module-load`)
-                         path = {
-                           'lua/?.lua',
-                           'lua/?/init.lua',
-                         },
-                       },
-                       -- Make the server aware of Neovim runtime files
-                       workspace = {
-                         checkThirdParty = false,
-                         library = {
-                           vim.env.VIMRUNTIME
-                           -- Depending on the usage, you might want to add additional paths
-                           -- here.
-                           -- '${3rd}/luv/library'
-                           -- '${3rd}/busted/library'
-                         }
-                         -- Or pull in all of 'runtimepath'.
-                         -- NOTE: this is a lot slower and will cause issues when working on
-                         -- your own configuration.
-                         -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-                         -- library = {
-                         --   vim.api.nvim_get_runtime_file('', true),
-                         -- }
-                       }
-                     })
-                   end,
-                   settings = {
-                     Lua = {}
-                   }
-                 })
-
+                require'lspconfig'.lua_ls.setup{}
             end,
         },
         {
