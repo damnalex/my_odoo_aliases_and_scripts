75e194bc65dceaf69f0486c206d0e6473b3879c8
Moens Alexandre <mao@odoo.com>
2026-06-05 14:15:51 +0200

    [perf] test-dump: don;t do the same work twice
    the dump safety check is already handled by oes

--- alias.sh
+++ alias.sh
@@ -680,8 +680,6 @@ test-dump() {
     # test-dump ([<db_name>] | <db_name>  [<path_to_dump_parent_directory>])
     local dump_parent_folder=${2:-$(pwd)}
     local dump_f=$dump_parent_folder/dump.sql
-    $PSS/test_dump_safety.py $dump_f --not-strict || return 1
-    echo "Safety check OK"
     local db_name=${1:-$(uuidgen | tr '[:upper:]' '[:lower:]')}
     oes restore-dump $db_name $dump_f --no-start
     # check for custom modules
